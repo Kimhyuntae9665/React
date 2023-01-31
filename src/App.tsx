fe552126033503454a88a4fa1d685f8f2705c4df
Kim <rlagusxo96652@naver.com>
2023-01-31 17:47:23 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 // 한줄 주석
 /*공간 주석 가능 */
 
-// java에서 method 만들 때에는 
+//! java에서 method 만들 때에는 
 // 접근제어자 기타제어자 반환타입 메서드명(파라미터)
 
-// typescript method 생성 방법은 
+//todo typescript method 생성 방법은 
 // (1)function 메서드명(파라미터[: 파라미터 타입])[: 반환타입] {...}
 
 // (2) 자바스크립트,타입스크립트는 메서드를 변수로 인식합니다 
@@ -38,6 +38,14 @@ function App() {
 
   // typescript에서의 비교연산자 
 
+  let a : any = 10;
+  let b : any = '10';
+
+
+  // == ,!= : 값만 비교 
+  //  === ,!== :값과 타입까지 비교  (>== - 이런 형태는 존재하지 않음 )
+  // alert(a == b);
+  alert(a===b);
 
   // typescript에서의 나눗셈 연산 
   let tmp6 = 10/3; // 3.33333
