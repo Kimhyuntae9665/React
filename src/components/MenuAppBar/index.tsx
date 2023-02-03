2b2fcb93a379216074341a7f0c9b110ca669a622
Kim <rlagusxo96652@naver.com>
2023-02-03 17:50:08 +0900

    miu 적용

---
+++ src/components/MenuAppBar/index.tsx
@@ -0,0 +1,147 @@
+import {useState,MouseEvent} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = [{title: 'Naver',viewValue: 'naverSignIn'},{title: 'Kakao',viewValue:'KakaoSIgnIn'}];
+// ! typescript에서는 함수를 매개변수로 쓸 수 있다 
+
+function MenuAppBar() {
+  // ! view state//
+  const [view, setView] = useState<string>('');
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+ 
+
+  const handleOpenNavMenu = (event:MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+ 
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {/* 
+              //? pages = [{'Naver','NaverSignIn'},{'Kakao','KakaoSignIn'}]
+              //? pages = [{title: 'Naver',viewValue: 'naverSignIn'},{title: 'Kakao',viewValue:'KakaoSIgnIn'}]
+              // ! 2차원 배열보다는 2번째 방법인 객체배열로 하는것이 더 직관적이고 다양한 타입이 올 수 있어서 장점이 더 많아 
+              */}
+
+              {pages.map((page) => (
+                // ! key 뒤에는 구분자로 구분 할 수 있는 인자를 써서 
+                <MenuItem key={page.title} onClick={()=>setView(page.viewValue)}>
+                  <Typography textAlign="center">{page.title}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.title}
+                onClick={()=>setView(page.title)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page.title}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton  sx={{ p: 0 }}>
+                <Avatar alt="Faker" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default MenuAppBar;
